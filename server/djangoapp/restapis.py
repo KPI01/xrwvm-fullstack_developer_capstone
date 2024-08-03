f64a4e68642559b308b6c037ac79705becd29954
KPI01 <jlrd.dev@gmail.com>
2024-08-03 09:35:27 -0400

    UPDT: implemented microservice

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,12 +11,35 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params += key + "=" + value + "&"
+    request_url = backend_url + endpoints + "?" + params
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+    print(f"GET from {request_url}")
 
-# def post_review(data_dict):
-# Add code for posting review
+    try:
+        response = request.get(request_url)
+        return response.json()
+    except:
+        print("Network exception ocurred")
+
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        reponse = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f'Unexpected {err=}, {type(err)=}')
+        print("Network error ocurred")
+
+def post_review(data_dict):
+    request_url = f"{backend_url}/insert_review"
+    try:
+        response = requests.post(request_url, json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception ocurred")
