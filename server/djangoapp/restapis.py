641090e3fe4f3ed050bb43291dac50695287b14c
KPI01 <jlrd.dev@gmail.com>
2024-08-25 04:55:44 -0400

    implemented carsInventory

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -8,6 +8,9 @@ load_dotenv()
 backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 print("restapis.py backend_url:", backend_url)
+searchcars_url = os.getenv(
+    'searchcars_url',
+    default="http://localhost:3050/")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
@@ -49,3 +52,22 @@ def post_review(data_dict):
         return response.json()
     except Exception as e:
         print(f"Network exception occurred [{e}]")
+
+
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
+    
+    request_url = searchcars_url + endpoint + "?" + params
+
+    print(f"GET from {request_url}")
+
+    try:
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as e:
+        print(f"Network exception ocurred [{e}]")
+    finally:
+        print("GET request call completed")
